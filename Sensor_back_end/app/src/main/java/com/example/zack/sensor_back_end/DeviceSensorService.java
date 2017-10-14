efa7a6e92b1477e009f028aa5438bddc0bc33086
deepforce <zyq81678593@126.com>
2017-10-14 13:00:12 +0800

    v2

--- Sensor_back_end/app/src/main/java/com/example/zack/sensor_back_end/DeviceSensorService.java
+++ Sensor_back_end/app/src/main/java/com/example/zack/sensor_back_end/DeviceSensorService.java
@@ -10,6 +10,9 @@ package com.example.zack.sensor_back_end;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -24,6 +27,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.os.Environment;
 import android.os.IBinder;
 import android.os.PowerManager;
@@ -35,10 +40,28 @@ import android.media.MediaRecorder;
 public class DeviceSensorService extends Service {
 //    public static final String ACTION = "com.example.zack.sensor_back_end.DeviceSensorService";
     private static final String TAG = "DeviceSensorService";
+    public static boolean recordering = false;
     private MediaRecorder recorder;
     Sensor sensorAcc, sensoGyros, sensoPress;
     SensorManager sm;
     WakeLock m_wklk;
+    public static Thread thread;
+
+    private static AudioRecord audioRecord;
+
+    private static String currentFilePath;
+
+    // 音频获取源
+    private static int audioSource = MediaRecorder.AudioSource.MIC;
+    // 设置音频采样率，44100是目前的标准
+    private static int sampleRateInHz = 44100;
+    // 设置音频的录制的声道CHANNEL_IN_STEREO为双声道，CHANNEL_CONFIGURATION_MONO为单声道
+    private static int channelConfig = AudioFormat.CHANNEL_IN_STEREO;
+    // 音频数据格式:PCM 16位每个样本。保证设备支持。PCM 8位每个样本。不一定能得到设备支持。
+    private static int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+    // 缓冲区字节大小
+    private static int bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz,
+            channelConfig, audioFormat);
 
 
 
@@ -58,15 +81,27 @@ public class DeviceSensorService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i(TAG, "Zack onStartCommand");
-        new Thread(new Runnable() {
-            @Override
+        class senThread implements Runnable {
+            private MyThead myThead;
             public void run() {
                 Log.d("DeviceSensorService", "executed at " + new Date().
                         toString());
+
+//
                 if (sm == null) {
                     sm = (SensorManager) getApplicationContext().getSystemService(
                             Context.SENSOR_SERVICE);
                 }
+                        /*
+                     * 最常用的一个方法 注册事件
+                     * 参数1 ：SensorEventListener监听器
+                     * 参数2 ：Sensor 一个服务可能有多个Sensor实现，此处调用getDefaultSensor获取默认的Sensor
+                     * 参数3 ：模式 可选数据变化的刷新频率，采样率
+                     * SENSOR_DELAY_FASTEST,100次左右
+                     * SENSOR_DELAY_GAME,50次左右
+                     * SENSOR_DELAY_UI,20次左右
+                     * SENSOR_DELAY_NORMAL,5次左右
+                     */
                 // 加速度感应器
                 Sensor sensorAcc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
                 // 陀螺仪
@@ -85,24 +120,20 @@ public class DeviceSensorService extends Service {
                 Sensor sensoTemp = sm.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
                 // 光
                 Sensor sensoLight = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
+
+
+
                 while (true) {
 
-
                     try {
-                    /*
-                     * 最常用的一个方法 注册事件
-                     * 参数1 ：SensorEventListener监听器
-                     * 参数2 ：Sensor 一个服务可能有多个Sensor实现，此处调用getDefaultSensor获取默认的Sensor
-                     * 参数3 ：模式 可选数据变化的刷新频率，采样率
-                     * SENSOR_DELAY_FASTEST,100次左右
-                     * SENSOR_DELAY_GAME,50次左右
-                     * SENSOR_DELAY_UI,20次左右
-                     * SENSOR_DELAY_NORMAL,5次左右
-                     */
-                        initializeAudio();
+//
 
+                        initializeAudio();
+                        recordering = true;
                         sm.registerListener(mySensorListener, sensorAcc,
-                            SensorManager.SENSOR_DELAY_NORMAL); //以普通采样率注册监听器
+                                SensorManager.SENSOR_DELAY_NORMAL); //以普通采样率注册监听器
                         sm.registerListener(mySensorListener, sensoGyros,
                                 SensorManager.SENSOR_DELAY_NORMAL);
                         sm.registerListener(mySensorListener, sensoPress,
@@ -123,66 +154,65 @@ public class DeviceSensorService extends Service {
                         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
                         m_wklk = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, DeviceSensorService.class.getName());
                         m_wklk.acquire();
-
-
+                        myThead = new MyThead();
+                        Thread thread = new Thread(myThead);
+                        thread.start();
                         Thread.sleep(5000);  // 延迟5秒
-                        recorder.stop();// 停止刻录
-                        // recorder.reset(); // 重新启动MediaRecorder.
-                        recorder.release(); // 刻录完成一定要释放资源
-
+                        recordering = false;
+                        thread.sleep(10 * 1000);
                         sm.unregisterListener(mySensorListener);
-                        m_wklk.release();
-//
-                        Thread.sleep(10 * 60 * 1000); // 两次收集间隔时间
+                        Thread.sleep(10 * 1000); // 两次收集间隔时间
                     } catch (InterruptedException e) {
                         System.out.println(e.getMessage());
-                        break;
+                        //break;
                     }
+                }
+            }
 
+
+            class MyThead implements Runnable {
+                public void run()
+                {
+                    writeDateTOFile();
+                    copyWaveFile(currentFilePath, currentFilePath.replace(".pcm", ".wav"));//给裸数据加上头文件
                 }
 
             }
-        }).start();
 
 
 
+
+        }
+        senThread mythread = new senThread();
+        thread = new Thread(mythread);
+        thread.start();
         return Service.START_STICKY;
     }
 
     private void initializeAudio() {
-        SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS ");
-        String date = sDateFormat.format(new java.util.Date());
-        recorder = new MediaRecorder();// new出MediaRecorder对象
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        // 设置MediaRecorder的音频源为麦克风
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-        // 设置MediaRecorder录制的音频格式
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-        // 设置MediaRecorder录制音频的编码为amr
-        String record_path = Environment.getExternalStorageDirectory()+ "/sdcard/records/";
+
+        String record_path = Environment.getExternalStorageDirectory()+ "/sdcard/data/" + Integer.toString(MainActivity.count) + "/records/";
         File file_record = new File(record_path);
         if(!file_record.exists()){
             file_record.mkdirs();
         }
 
-        recorder.setOutputFile(record_path + date + ".amr");
-        // 设置录制好的音频文件保存路径
-        try {
-            recorder.prepare();// 准备录制
-            recorder.start();// 开始录制
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS ");
+        String date = sDateFormat.format(new java.util.Date());
+
+        currentFilePath = record_path + date + ".pcm";
+        audioRecord = new AudioRecord(audioSource, sampleRateInHz,
+                channelConfig, audioFormat, bufferSizeInBytes);
+        audioRecord.startRecording();
+
     }
+
+
 
     public void onDestroy() {
         Log.i(TAG, "Stop!");
-        recorder.stop();// 停止刻录
-
-        recorder.release(); // 刻录完成一定要释放资源
+        recordering = false;
         if (sm != null) {
             sm.unregisterListener(mySensorListener);
             mySensorListener = null;
@@ -192,11 +222,130 @@ public class DeviceSensorService extends Service {
             m_wklk.release();
             m_wklk = null;
         }
-        Thread.interrupted();
+
+
 
     };
 
+    private static void writeDateTOFile() {
+        // new一个byte数组用来存一些字节数据，大小为缓冲区大小
+        final byte[] audiodata = new byte[bufferSizeInBytes];
+        FileOutputStream fos = null;
+        try {
+            File file = new File(currentFilePath);
+            if (file.exists()) {
+                file.delete();
+            }
 
+            file.createNewFile();
+
+            fos = new FileOutputStream(file);// 建立一个可存取字节的文件
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while (recordering == true) {
+            final int readsize = audioRecord.read(audiodata, 0, bufferSizeInBytes);
+            if (AudioRecord.ERROR_INVALID_OPERATION != readsize) {
+                try {
+                    fos.write(audiodata);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        try {
+            fos.close();// 关闭写入流
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 这里得到可播放的音频文件
+    private static void copyWaveFile(String inFilename, String outFilename) {
+        FileInputStream in = null;
+        FileOutputStream out = null;
+        long totalAudioLen = 0;
+        long totalDataLen = totalAudioLen + 36;
+        long longSampleRate = sampleRateInHz;
+        int channels = 2;
+        long byteRate = 16 * sampleRateInHz * channels / 8;
+        byte[] data = new byte[bufferSizeInBytes];
+        try {
+            in = new FileInputStream(inFilename);
+            out = new FileOutputStream(outFilename);
+            totalAudioLen = in.getChannel().size();
+            totalDataLen = totalAudioLen + 36;
+            WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+                    longSampleRate, channels, byteRate);
+            while (in.read(data) != -1) {
+                out.write(data);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 这里提供一个头信息。插入这些信息就可以得到可以播放的文件。
+     * 为我为啥插入这44个字节，这个还真没深入研究，不过你随便打开一个wav
+     * 音频的文件，可以发现前面的头文件可以说基本一样哦。每种格式的文件都有
+     * 自己特有的头文件。
+     */
+    private static void WriteWaveFileHeader(FileOutputStream out, long totalAudioLen,
+                                            long totalDataLen, long longSampleRate, int channels, long byteRate)
+            throws IOException {
+        byte[] header = new byte[44];
+        header[0] = 'R'; // RIFF/WAVE header
+        header[1] = 'I';
+        header[2] = 'F';
+        header[3] = 'F';
+        header[4] = (byte) (totalDataLen & 0xff);
+        header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+        header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+        header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+        header[8] = 'W';
+        header[9] = 'A';
+        header[10] = 'V';
+        header[11] = 'E';
+        header[12] = 'f'; // 'fmt ' chunk
+        header[13] = 'm';
+        header[14] = 't';
+        header[15] = ' ';
+        header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+        header[17] = 0;
+        header[18] = 0;
+        header[19] = 0;
+        header[20] = 1; // format = 1
+        header[21] = 0;
+        header[22] = (byte) channels;
+        header[23] = 0;
+        header[24] = (byte) (longSampleRate & 0xff);
+        header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+        header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+        header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+        header[28] = (byte) (byteRate & 0xff);
+        header[29] = (byte) ((byteRate >> 8) & 0xff);
+        header[30] = (byte) ((byteRate >> 16) & 0xff);
+        header[31] = (byte) ((byteRate >> 24) & 0xff);
+        header[32] = (byte) (2 * 16 / 8); // block align
+        header[33] = 0;
+        header[34] = 16; // bits per sample
+        header[35] = 0;
+        header[36] = 'd';
+        header[37] = 'a';
+        header[38] = 't';
+        header[39] = 'a';
+        header[40] = (byte) (totalAudioLen & 0xff);
+        header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+        header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+        header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+        out.write(header, 0, 44);
+    }
 
     /*
      * SensorEventListener 接口的实现，需要实现两个方法
--- Sensor_back_end/app/src/main/java/com/example/zack/sensor_back_end/MainActivity.java
+++ Sensor_back_end/app/src/main/java/com/example/zack/sensor_back_end/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.zack.sensor_back_end;
 
+import android.content.DialogInterface;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends Activity
 {
     private Button startBtn;
     private Button stopBtn;
+    public static int count;
+
+    private CreateUserPopWin createUserPopWin;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -38,9 +52,14 @@ public class MainActivity extends Activity
             {
                 case R.id.startBtn:
                     System.out.println("Start DeviceSensor service...");
+                    calcount();
+                    new AlertDialog.Builder(MainActivity.this).setTitle("消息")//设置对话框标题
+                            .setMessage("开始收集！")//设置显示的内容
+                    .show();//在按键响应事件中显示此对话框
                     startService(intent);
                     break;
                 case R.id.stopBtn:
+                    showEditPopWin();
                     System.out.println("Stop DeviceSensor service...");
                     stopService(intent);
                     break;
@@ -50,5 +69,73 @@ public class MainActivity extends Activity
         }
     };
 
+    public void showEditPopWin() {
+        createUserPopWin = new CreateUserPopWin(this,onClickListener);
+        createUserPopWin.showAtLocation(findViewById(R.id.main_view), Gravity.CENTER, 0, 0);
+    }
 
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.btn_save_pop:
+
+                    String name1 = createUserPopWin.text_name.getText().toString().trim();
+                    String mobile1 = createUserPopWin.text_mobile.getText().toString().trim();
+                    String info1 = createUserPopWin.text_info.getText().toString().trim();
+
+
+                    System.out.println(name1+"——"+mobile1+"——"+info1);
+                    writeUserprofile(name1, mobile1, info1);
+
+                    createUserPopWin.dismiss();
+                    Thread.interrupted();
+                    DeviceSensorService.thread.interrupt();
+                    DeviceSensorService.thread.stop();
+                    break;
+            }
+        }
+    };
+
+    private void calcount() {
+        for (count = 1; count <= 1000; count++) {
+            String record_path = Environment.getExternalStorageDirectory() + "/sdcard/data/";
+            File file_record = new File(record_path + Integer.toString(count) + "/");
+            if (!file_record.exists()) {
+                file_record.mkdirs();
+                break;
+            }
+
+        }
+    }
+
+    private void writeUserprofile(String name, String mobile, String info) {
+        String record_path = Environment.getExternalStorageDirectory() + "/sdcard/data/";
+        File file_record = new File(record_path + Integer.toString(count) + "/Userprofile.txt");
+        SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS ");
+        String date = sDateFormat.format(new java.util.Date());
+        try {
+
+            FileWriter filerWriter = new FileWriter(file_record, true);//后面这个参数代表是不是要接上文件中原来的数据，不进行覆盖
+            BufferedWriter bufWriter = new BufferedWriter(filerWriter);
+            bufWriter.write("姓名:" + name);
+            bufWriter.newLine();
+            bufWriter.write("手机:" + mobile);
+            bufWriter.newLine();
+            bufWriter.write("标签:" + info);
+            bufWriter.newLine();
+            bufWriter.write("时间:" + date);
+            bufWriter.close();
+            filerWriter.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
